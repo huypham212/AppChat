3794a3fd32e96e986450d9b2107e028d094f73ac
louisleo2000 <75025995+louisleo2000@users.noreply.github.com>
2021-09-14 22:30:10 +0700

    test gửi tin trong scr chat

---
+++ components/a.js
@@ -0,0 +1,17 @@
+[
+  {
+    avatar:
+      'https://anhdep123.com/wp-content/uploads/2021/05/hinh-avatar-trang.jpg',
+    email: 'hai123@gmail.com',
+    isOnline: true,
+    name: 'Chu Thị Thu Hải',
+  },
+  {
+    avatar:
+      'https://scontent.fhan14-2.fna.fbcdn.net/v/t1.6435-1/p200x200/90084631_2545692372385647_1868446112474464256_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=7206a8&_nc_ohc=Uo3khqxm-yUAX8zQ7sw&_nc_ht=scontent.fhan14-2.fna&oh=696f885a95668493b6ab1cb94c432c37&oe=6167AD69',
+    email: 'uyen123@gmail.com',
+    isOnline: true,
+    messages: {fsdafsad: [Object]},
+    name: 'Ái Uyên',
+  },
+];
