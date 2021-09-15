594c9df59ee7927a38946daee9c51b784988fc59
louisleo2000 <75025995+louisleo2000@users.noreply.github.com>
2021-09-15 11:32:27 +0700

    update listchat and chatscr

--- components/a.js
+++ components/a.js
@@ -1,17 +1,49 @@
-[
+Item: [
   {
+    _id: 'UvqLPaqWNNSkpBOOfK725Awybus2',
+    avatar:
+      'https://anhdep123.com/wp-content/uploads/2021/05/hinh-avatar-trang.jpg',
+    isOnline: true,
+    lastMess: 'huhu',
+    name: 'Chu Thị Thu Hải',
+  },
+  {
+    _id: '6yUNa6fSceZhNckFZuokTSzgXKG2',
+    avatar:
+      'https://scontent.fhan14-2.fna.fbcdn.net/v/t1.6435-1/p200x200/90084631_2545692372385647_1868446112474464256_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=7206a8&_nc_ohc=Uo3khqxm-yUAX8zQ7sw&_nc_ht=scontent.fhan14-2.fna&oh=696f885a95668493b6ab1cb94c432c37&oe=6167AD69',
+    isOnline: true,
+    lastMess: 'helloban',
+    name: 'Ái Uyên',
+  },
+];
+List[
+  ({
+    _id: 'UvqLPaqWNNSkpBOOfK725Awybus2',
     avatar:
       'https://anhdep123.com/wp-content/uploads/2021/05/hinh-avatar-trang.jpg',
     email: 'hai123@gmail.com',
     isOnline: true,
+    messages: {
+      '-MjbIseZyefehT7qmh2S': [Object],
+      '-MjbVrrOzC0jzzMIUukn': [Object],
+      '-MjbY5BUJhO7azJdFPP2': [Object],
+      '-MjbYGXRGdUty3CWzjn3': [Object],
+      '-MjbYT1kn8izEHHqumPx': [Object],
+      '-MjbZ8sjJNlsj5kr5C2x': [Object],
+    },
     name: 'Chu Thị Thu Hải',
   },
   {
+    _id: '6yUNa6fSceZhNckFZuokTSzgXKG2',
     avatar:
       'https://scontent.fhan14-2.fna.fbcdn.net/v/t1.6435-1/p200x200/90084631_2545692372385647_1868446112474464256_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=7206a8&_nc_ohc=Uo3khqxm-yUAX8zQ7sw&_nc_ht=scontent.fhan14-2.fna&oh=696f885a95668493b6ab1cb94c432c37&oe=6167AD69',
     email: 'uyen123@gmail.com',
     isOnline: true,
-    messages: {fsdafsad: [Object]},
+    messages: {
+      '-MjbUaGIgVUc5IXxCXTc': [Object],
+      '-MjbZN9I3hdtaTlo8Bk-': [Object],
+      '-MjbZOu-rHqMSt18GO9c': [Object],
+    },
     name: 'Ái Uyên',
-  },
+  })
 ];
